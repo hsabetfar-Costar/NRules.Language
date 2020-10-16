82d83fb06942a2d8a1b1c26a1d1991f5e163b86e
hsabetfar-Costar <72951010+hsabetfar-Costar@users.noreply.github.com>
2020-10-16 10:18:03 -0700

    Update TypeMapExtensions.cs

--- src/NRules.RuleSharp/NRules.RuleSharp/TypeMapExtensions.cs
+++ src/NRules.RuleSharp/NRules.RuleSharp/TypeMapExtensions.cs
@@ -19,6 +19,18 @@
             typeMap.AddAlias("short", "System.Int16");
             typeMap.AddAlias("ushort", "System.UInt16");
             typeMap.AddAlias("string", "System.String");
+            typeMap.AddAlias("int?", "System.Int32?");
+            typeMap.AddAlias("bool?", "System.Boolean?");
+            typeMap.AddAlias("byte?", "System.Byte?");
+            typeMap.AddAlias("sbyte?", "System.SByte?");
+            typeMap.AddAlias("decimal?", "System.Decimal?");
+            typeMap.AddAlias("double?", "System.Double?");
+            typeMap.AddAlias("float?", "System.Single?");
+            typeMap.AddAlias("uint?", "System.UInt32?");
+            typeMap.AddAlias("long?", "System.Int64?");
+            typeMap.AddAlias("ulong?", "System.UInt64?");
+            typeMap.AddAlias("short?", "System.Int16?");
+            typeMap.AddAlias("ushort?", "System.UInt16?");            
         }
     }
 }
