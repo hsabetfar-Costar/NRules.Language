6ccc8d8ef0794c07c977d10c47519345bc1ac3c2
hsabetfar-Costar <72951010+hsabetfar-Costar@users.noreply.github.com>
2020-10-16 10:24:01 -0700

    Add files via upload

---
+++ src/NRules.RuleSharp/NRules.RuleSharp/ExtensionMethods.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NRules.RuleSharp
+{
+    internal static class ExtensionMethods
+    {
+        public static Type GetNullabeType(this Type t)
+        {
+             return typeof(Nullable<>).MakeGenericType(t);
+        }
+    }
+
+}
